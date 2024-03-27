e4207cb5970d87b2e5f6dbb39c125be672b2ee8f
sofpav <sofyapavlova12@gmail.com>
2024-03-27 21:20:08 +0300

    kommit

--- praktika 27.03.2024 (wednesday).py
+++ praktika 27.03.2024 (wednesday).py
@@ -1,10 +1,14 @@
-list_ = [1, 2, 3, 4, 5]
-a = list_[len(list_) // 3]
-print("а первоначальная", a)
-for b in range(len(list_)):
-    print(b)
-    if list_[b] < a:
-        a = list_[b]
-        print(a)
+# list_ = [1, 2, 3, 4, 5]
+# a = list_[len(list_) // 3]
+# print("а первоначальная", a)
+# for b in range(len(list_)):
+#     print(b)
+#     if list_[b] < a:
+#         a = list_[b]
+#         print(a)
+#
+# print(a)
 
-print(a)
+list_ = [1, 2, 3, 4, 5]
+for value in list_:
+    print(value)
