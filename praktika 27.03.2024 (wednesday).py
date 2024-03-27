e4743bb94d2ac8c8a904d05f2268e0b19b2aeb2a
sofpav <sofyapavlova12@gmail.com>
2024-03-27 20:55:35 +0300

    kommit

--- praktika 27.03.2024 (wednesday).py
+++ praktika 27.03.2024 (wednesday).py
@@ -1,3 +1,10 @@
-student_scores = {"John": 85, "Jane": 92, "Alice": 78}
-for score in student_scores.values():
-    print(score)
+list_ = [1, 2, 3, 4, 5]
+a = list_[len(list_) // 3]
+print("а первоначальная", a)
+for b in range(len(list_)):
+    print(b)
+    if list_[b] < a:
+        a = list_[b]
+        print(a)
+
+print(a)
